2ea6f89099f2135cbbfb5cb1a367913ed301188a
Gustavo Santana Coelho <gustavo_13roo@hotmail.com>
2019-04-01 22:15:21 -0400

    Iniciando controlador Fazenda

---
+++ application/views/pages/fazenda/index.php
@@ -0,0 +1,13 @@
+<div class="row">
+    <div class="col-xs-12 col-sm-12 col-md-12">
+        <div class="card">
+            <div class="card-body">
+                <div class="card-title">
+                    <span class="titulo">
+                        <center>Fazenda Com Mural</center>
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
